a0b06a133d970c32f0cba0f50a8bca8c09c9e382
theussilvas <matheussilva540@yahoo.com>
2025-06-18 10:34:04 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Iphone iphone = new Iphone("84999999", "16", "023012001239", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        
+        iphone.InstalarAplicativo("Whatsapp");
+        
+        Nokia nokia = new Nokia("84999999", "ASHA", "023012001239", 128);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Whatsapp");
+        
+
+    }
+}
